54c18f368a25249a53ef73b7e060493fc291e5ca
Vinit Kumar <vinit.ad.ad@gmail.com>
2026-07-25 10:58:00 +0530

    update BASE_URL in setting.php

--- setting.php
+++ setting.php
@@ -1,8 +1,13 @@
 <?php
 session_start();
 
-define('BASE_URL', 'http://local.php/miniEcommerce/');
 define('BASE_PATH', __DIR__ . '/');
+
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+$host = $_SERVER['HTTP_HOST'];
+$scriptDir = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
+define('BASE_URL', $protocol . $host . $scriptDir . '/');
+
 define('API_ROOT', 'https://vinit.convertfileplus.com/api/');
 define('API_URL', 'https://vinit.convertfileplus.com/storage/');
 // define('API_ROOT', 'https://vecommerce.up.railway.app/api/v1/');
